5762c23b3c94c3291246c55f47583d359e214871
Filip Dabkowski <filip.dabkowski@gmail.com>
2026-03-21 15:57:46 +0100

    remove dev level logging

--- backend_django/player/views.py
+++ backend_django/player/views.py
@@ -21,7 +21,6 @@ class PlayerMeView(APIView):
 	permission_classes = [IsAuthenticated]
 
 	def get(self, request):
-		print(request.user.username)
 		player = get_object_or_404(Player, user__username=request.user.username)
 
 		serializer = PlayerSerializer(player)
